4243d13928ca068e19a627eec9a3996dce6faebf
王赢辉 <m17678319501@163.com>
2020-06-29 19:56:06 +0800

    删除了ID

--- utils/appConfig.js
+++ utils/appConfig.js
@@ -1,9 +1,11 @@
+/** 这里涉及到一些隐私安全的问题，希望大家自行配置 */
+
 // 用户ID
-const USER_ID = 'bea07868615346c97cca'
+const USER_ID = ''
 // 广告ID
-const BOARD_ID = '2e0i841ql0t62hb15f'
+const BOARD_ID = ''
 // 查询表名
-const QUERY_TABLE = 'query_table'
+const QUERY_TABLE = ''
 
 module.exports = {
     USER_ID: USER_ID,
